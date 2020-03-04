886c5375e6e802e55c4319d3cc98933bc47cfb9d
Niek Palm <dev.npalm@gmail.com>
2020-03-04 20:52:26 +0100

    Cleanup

--- apollo-link-rest/src/index.ts
+++ apollo-link-rest/src/index.ts
@@ -2,16 +2,9 @@ import { ApolloServer, gql } from 'apollo-server';
 import { RootSchema } from './schema/RootSchema';
 import { TalkResolver } from './resolvers/talkResolver'
 
-// import PolyfillHeaders = require("fetch-headers");
-// if (global.Headers == null) {
-//   global.Headers = PolyfillHeaders;
-// }
-
-//global.Headers = global.Headers || require("fetch-headers");
-
 const main = async () => {
   const server = await new ApolloServer({ typeDefs: RootSchema, resolvers: TalkResolver }).listen();
-  console.log(`💩 Server started at: ${server.url}`);
+  console.log(`Server started at: ${server.url}`);
 };
 
 main();
